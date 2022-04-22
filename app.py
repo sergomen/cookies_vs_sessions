f5cc0d99bc05060148ea7d89b2e9c3d9cc9a0abe
Sergei Iu <serg_omen@mail.ru>
2022-04-23 00:12:07 +0300

    Addit .gitignore at first and requirements.txt

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
-s
+from flask import Flask, render_template, request, make_response
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/setcookie', methods = ['POST'])
+def setcookie1():
+    if request.method == 'POST':
+        user = request.form['nm']
+
+        resp = make_response(render_template('readcookie.html'))
+        resp.set_cookie('userID', user)
+
+        return resp
+
+@app.route('/getcookie', methods = ['GET'])
+def getcookie():
+    name = request.cookies.get('userID')
+    return '<h1>Welcome ' + name + '</h1>'
